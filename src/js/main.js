147ed3edd94f8652d2cf653ab2d147901cb36311
Vlad Tepes <vg.email.2@gmail.com>
2018-06-15 20:25:43 +0300

    intro added, fullpage added, about started

--- src/js/main.js
+++ src/js/main.js
@@ -5,4 +5,16 @@ $(document).ready(function () {
         svg4everybody();
     }());
 
+    //----------------------<<one page scroll>>----------------------\\
+    (function () {
+        $('#fullpage').fullpage({
+
+            anchors:['first', 'second', 'third'],
+            navigation: false,
+            scrollBar: false,
+            scrollOverflow: true
+
+        });
+    }());
+
 });
