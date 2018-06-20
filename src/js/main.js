abccec94f99efdb0e1fe50d00ad9e04393c535b0
Vlad Tepes <vg.email.2@gmail.com>
2018-06-20 18:38:27 +0300

    nanogallery styling finished, code refactored

--- src/js/main.js
+++ src/js/main.js
@@ -28,7 +28,29 @@ $(document).ready(function () {
         };
 
         $("#nanogallery2").nanogallery2( {
-            <!-- ### gallery settings ### -->
+
+            galleryDisplayMode: 'moreButton',
+            galleryDisplayMoreStep: 2,
+            galleryMaxRows: 2,
+
+            itemsBaseURL:     '../assets/img/portfolio/',
+            items: [
+                { src: 'portfolio1.jpg', srct: 'thumb1.jpg'},
+                { src: 'portfolio2.jpg', srct: 'thumb2.jpg'},
+                { src: 'portfolio3.jpg', srct: 'thumb3.jpg'},
+                { src: 'portfolio4.jpg', srct: 'thumb4.jpg'},
+
+                { src: 'portfolio4.jpg', srct: 'thumb4.jpg'},
+                { src: 'portfolio3.jpg', srct: 'thumb3.jpg'},
+                { src: 'portfolio2.jpg', srct: 'thumb2.jpg'},
+                { src: 'portfolio1.jpg', srct: 'thumb1.jpg'},
+
+                { src: 'portfolio2.jpg', srct: 'thumb2.jpg'},
+                { src: 'portfolio1.jpg', srct: 'thumb1.jpg'},
+                { src: 'portfolio4.jpg', srct: 'thumb4.jpg'},
+                { src: 'portfolio3.jpg', srct: 'thumb3.jpg'}
+            ],
+
             thumbnailHeight:  300,
             thumbnailWidth:   300,
             thumbnailAlignment: 'center',
@@ -36,35 +58,17 @@ $(document).ready(function () {
             thumbnailGutterHeight: 0,
             thumbnailBorderHorizontal: 0,
             thumbnailBorderVertical: 0,
-            itemsBaseURL:     '../assets/img/portfolio/',
-
-            <!-- ### gallery content ### -->
-            items: [
-                { src: 'portfolio1.jpg', srct: 'thumb1.jpg'},
-                { src: 'portfolio1.jpg', srct: 'thumb1.jpg'},
-                { src: 'portfolio1.jpg', srct: 'thumb1.jpg'},
-                { src: 'portfolio1.jpg', srct: 'thumb1.jpg'},
-                { src: 'portfolio1.jpg', srct: 'thumb1.jpg'},
-                { src: 'portfolio1.jpg', srct: 'thumb1.jpg'},
-                { src: 'portfolio1.jpg', srct: 'thumb1.jpg'},
-                { src: 'portfolio1.jpg', srct: 'thumb1.jpg'},
-                { src: 'portfolio1.jpg', srct: 'thumb1.jpg'},
-                { src: 'portfolio1.jpg', srct: 'thumb1.jpg'},
-                { src: 'portfolio1.jpg', srct: 'thumb1.jpg'}
-            ],
             thumbnailDisplayTransition: 'scaleUp',
             thumbnailDisplayTransitionDuration: 1000,
-            thumbnailHoverEffect2: {
-                name:           'imageScale150Outside',
-                duration:       10000,
-                durationBack:   1000,
-            },
-            galleryDisplayMode: 'moreButton',
-            galleryDisplayMoreStep: 2,
-            galleryMaxRows: 2,
             thumbnailLabel: {
                 display: false
             },
+            thumbnailHoverEffect2: {
+                name:           'imageScale150Outside',
+                duration:       10000,
+                durationBack:   300,
+            },
+
             viewerToolbar:    {
                 display: false,
                 standard:  "",
@@ -74,12 +78,15 @@ $(document).ready(function () {
                 topLeft: "pageCounter",
                 topRight: "closeButton"
             },
+            viewerTheme: custTheme,
+
             icons: {
                 buttonClose: "<svg class=\"portfolio__svg\"><use xlink:href=\"assets/img/svg/sprite/svg-sprite.svg#close\"></use></svg>",
                 viewerImgPrevious: "<svg class=\"portfolio__svg portfolio__svg_size\"><use xlink:href=\"assets/img/svg/sprite/svg-sprite.svg#prev\"></use></svg>",
-                viewerImgNext: "<svg class=\"portfolio__svg portfolio__svg_size\"><use xlink:href=\"assets/img/svg/sprite/svg-sprite.svg#next\"></use></svg>"
-            },
-            viewerTheme: custTheme
+                viewerImgNext: "<svg class=\"portfolio__svg portfolio__svg_size\"><use xlink:href=\"assets/img/svg/sprite/svg-sprite.svg#next\"></use></svg>",
+                galleryMoreButton: "<svg class=\"portfolio__svg-moreBtn\"><use xlink:href=\"assets/img/svg/sprite/svg-sprite.svg#photo\"></use></svg>"
+            }
+
         });
 
     }());
