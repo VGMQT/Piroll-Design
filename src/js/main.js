88dbe1c2dea084ee4811cf5cc41821cf8837b0f9
Vlad Tepes <vg.email.2@gmail.com>
2018-07-07 17:25:20 +0300

    video added, pros added

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,19 @@
 $(document).ready(function () {
 
     //----------------------<<svg for ie>>----------------------\\
-    (function () {
-        svg4everybody();
-    }());
+    svg4everybody();
 
     //----------------------<<one page scroll>>----------------------\\
-    (function () {
-        $('#fullpage').fullpage({
+    $('#fullpage').fullpage({
 
-            anchors:['first', 'second', 'third'],
-            navigation: false,
-            scrollBar: false,
-            autoScrolling: false,
-            fitToSection: true,
-            fitToSectionDelay: 1000
+        anchors:['first', 'second', 'third'],
+        navigation: false,
+        scrollBar: false,
+        autoScrolling: false,
+        fitToSection: true,
+        fitToSectionDelay: 1000
 
-        });
-    }());
+    });
 
     //----------------------<<gallery>>----------------------\\
     (function () {
