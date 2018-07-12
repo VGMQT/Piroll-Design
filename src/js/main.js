8578b4fca4bee881865795ad74fb8ece8d721cfe
Vlad Tepes <vg.email.2@gmail.com>
2018-07-12 14:21:11 +0300

    contact page done

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,44 @@
+//----------------------<<google map>>----------------------\\
+function initMap() {
+
+    const myLatLng = {
+        lat: 40.798054, lng: -74.064936
+    };
+
+    const myStyle = [{
+        featureType: "all",
+        elementType: "all",
+        stylers: [{
+            saturation: -100
+        }]
+    }];
+
+    const map = new google.maps.Map(document.getElementById('map'), {
+        center: myLatLng,
+        zoom: 16,
+        disableDefaultUI: true,
+        zoomControl: true,
+        mapTypeControlOptions: {
+            mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'tehgrey']
+        }
+    });
+
+    const mapType = new google.maps.StyledMapType(myStyle, {
+        name:"Greyscale"
+    });
+
+    map.mapTypes.set('tehgrey', mapType);
+    map.setMapTypeId('tehgrey');
+
+    const marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: "Piroll Design",
+        icon: "assets/img/svg/marker.svg"
+    });
+}
+
+//----------------------<<ready functions>>----------------------\\
 $(document).ready(function () {
 
     //----------------------<<svg for ie>>----------------------\\
